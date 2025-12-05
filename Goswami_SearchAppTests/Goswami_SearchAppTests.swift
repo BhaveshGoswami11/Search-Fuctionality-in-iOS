6c6e05529dda8e88408361d06b4adf362609e38c
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-12-05 16:08:53 -0600

    Create Goswami_SearchAppTests.swift

---
+++ Goswami_SearchAppTests/Goswami_SearchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Goswami_SearchAppTests.swift
+//  Goswami_SearchAppTests
+//
+//  Created by Bhavesh on 10/2/25.
+//
+
+import Testing
+@testable import Goswami_SearchApp
+
+struct Goswami_SearchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
